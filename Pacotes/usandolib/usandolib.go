373edc21a3771eefcf0353f1d2b1f4c218173971
Valéria <valeria.vargas@compasso.com.br>
2019-10-30 15:38:59 -0300

    Golang: Pacotes

---
+++ Pacotes/usandolib/usandolib.go
@@ -0,0 +1,13 @@
+//go get -u github.com/ValchanOficial/Golang/Pacotes/goarea
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/ValchanOficial/Golang/Pacotes/goarea"
+)
+
+func main() {
+	fmt.Println(goarea.Circ(4.0))
+}
